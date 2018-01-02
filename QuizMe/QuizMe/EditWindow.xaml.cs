f50e43171e03cdd5bf361ba0884b10e0355a0876
KevinMech <kevin.mellachew@gmail.com>
2018-01-02 07:26:15 -0500

    Add datatable for datagrid to work with when EditWindow loads

--- QuizMe/QuizMe/EditWindow.xaml.cs
+++ QuizMe/QuizMe/EditWindow.xaml.cs
@@ -26,15 +26,33 @@ namespace QuizMe
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Return to previous window when this window closes
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">event</param>
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             ListWindow listWindow = new ListWindow();
             listWindow.Show();
         }
 
+        /// <summary>
+        /// Create and fill the datatable when window loads
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">event</param>
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
+            DataTable dataTable = new DataTable("Quiz");
+            DataColumn dataColumn = new DataColumn("Question");
+            dataTable.Columns.Add("Question", typeof(string));
+            dataTable.Columns.Add("Answer", typeof(string));
+            dataTable.Columns.Add("Option 1", typeof(string));
+            dataTable.Columns.Add("Option 2", typeof(string));
+            dataTable.Columns.Add("Option 3", typeof(string));
+            dataTable.NewRow();
+            gridQuizForm.DataContext = dataTable;
         }
     }
 }
